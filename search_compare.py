d04c04fa4b20f1958c21c3d8339cb4377ec5359d
jadahbytes <jadahbytes@gmail.com>
2022-02-23 21:45:44 -0500

    Assignment #4 Completed

--- search_compare.py
+++ search_compare.py
@@ -1,34 +1,190 @@
-import argparse
-# other imports go here
-
+import time
 import random
 
 def get_me_random_list(n):
-    """Generate list of n elements in random order
-    
-    :params: n: Number of elements in the list
-    :returns: A list with n elements in random order
-    """
     a_list = list(range(n))
     random.shuffle(a_list)
     return a_list
-    
-def sequential_search(a_list,item):
-    pass
 
+def sequential_search(a_list,item):
+    pos = 0
+    found = False
+
+    while pos < len(a_list) and not found:
+        if a_list[pos] == item:
+            found = True
+        else:
+            pos = pos + 1
+    return found
 
 def ordered_sequential_search(a_list,item):
-    pass
+    pos = 0
+    found = False
+    stop = False
+
+    while pos < len(a_list) and not found and not stop:
+        if a_list[pos] == item:
+            found = True
+        else:
+            if a_list[pos] > item:
+                stop = True
+            else:
+                pos = pos + 1
+    return found
 
 
 def binary_search_iterative(a_list,item):
-    pass
-    
-    
-def binary_search_recursive(a_list,item):
-    pass
+    first = 0
+    last = len(a_list) - 1
+    found = False
 
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if a_list[midpoint] == item:
+            found = True
+        else:
+            if item < a_list[midpoint]:
+                last = midpoint - 1
+            else:
+                first = midpoint + 1
+    return found
+
+
+def binary_search_recursive(a_list,item):
+    if len(a_list) == 0:
+        return False
+    else:
+        midpoint = len(a_list) // 2
+    if a_list[midpoint] == item:
+        return True
+    else:
+        if item < a_list[midpoint]:
+            return binary_search_recursive(a_list[:midpoint], item)
+        else:
+            return binary_search_recursive(a_list[midpoint + 1:], item)
 
 if __name__ == "__main__":
-    """Main entry point"""
-    pass
+# Sequential Search Execution Time Averages
+    total_time_sequential_search500 = 0
+    for i in range(100):
+        mylist = get_me_random_list(500)
+        start = time.time()
+        sorted_list = sequential_search(mylist, -1)
+        time_spent = time.time() - start
+        total_time_sequential_search500 += time_spent
+    avg_time = total_time_sequential_search500 / 100
+    print(f"Sequential Search with 500 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_sequential_search1000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(1000)
+        start = time.time()
+        sorted_list = sequential_search(mylist, -1)
+        time_spent = time.time() - start
+        total_time_sequential_search1000 += time_spent
+    avg_time = total_time_sequential_search1000 / 100
+    print(f"Sequential Search with 1000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_sequential_search5000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(5000)
+        start = time.time()
+        sorted_list = sequential_search(mylist, -1)
+        time_spent = time.time() - start
+        total_time_sequential_search5000 += time_spent
+    avg_time = total_time_sequential_search5000 / 100
+    print(f"Sequential Search with 5000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    # Ordered Sequential Search Execution Time Averages
+    total_time_ordered_sequential_search500 = 0
+    for i in range(100):
+        mylist = get_me_random_list(500)
+        start = time.time()
+        sorted_list = ordered_sequential_search(mylist, -1)
+        time_spent = time.time() - start
+        total_time_ordered_sequential_search500 += time_spent
+    avg_time = total_time_ordered_sequential_search500 / 100
+    print(f"Ordered Sequential Search with 500 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_ordered_sequential_search1000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(1000)
+        start = time.time()
+        sorted_list = ordered_sequential_search(mylist, -1)
+        time_spent = time.time() - start
+        total_time_ordered_sequential_search1000 += time_spent
+    avg_time = total_time_ordered_sequential_search1000 / 100
+    print(f"Ordered Sequential Search with 1000 elements took {avg_time:10.7f} seconds to run, on average")
+
+
+    total_time_ordered_sequential_search5000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(5000)
+        start = time.time()
+        sorted_list = ordered_sequential_search(mylist, -1)
+        time_spent = time.time() - start
+        total_time_ordered_sequential_search5000 += time_spent
+    avg_time = total_time_ordered_sequential_search5000 / 100
+    print(f"Ordered Sequential Search with 5000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    # Binary Search Iterative Execution Time Averages
+    total_time_binary_search_iterative500 = 0
+    for i in range(100):
+        mylist = get_me_random_list(500)
+        start = time.time()
+        sorted_list = binary_search_iterative(mylist, -1)
+        time_spent = time.time() - start
+        total_time_binary_search_iterative500 += time_spent
+    avg_time = total_time_binary_search_iterative500 / 100
+    print(f"Binary Search Iterative with 500 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_binary_search_iterative1000= 0
+    for i in range(100):
+        mylist = get_me_random_list(1000)
+        start = time.time()
+        sorted_list = binary_search_iterative(mylist, -1)
+        time_spent = time.time() - start
+        total_time_binary_search_iterative1000 += time_spent
+    avg_time = total_time_binary_search_iterative1000 / 100
+    print(f"Binary Search Iterative with 1000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_binary_search_iterative5000= 0
+    for i in range(100):
+        mylist = get_me_random_list(5000)
+        start = time.time()
+        sorted_list = binary_search_iterative(mylist, -1)
+        time_spent = time.time() - start
+        total_time_binary_search_iterative5000 += time_spent
+    avg_time = total_time_binary_search_iterative5000 / 100
+    print(f"Binary Search Iterative with 5000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    # Binary Search recursive Execution Time Averages
+    total_time_binary_search_recursive500 = 0
+    for i in range(100):
+        mylist = get_me_random_list(500)
+        start = time.time()
+        sorted_list = binary_search_recursive(mylist, -1)
+        time_spent = time.time() - start
+        total_time_binary_search_recursive500 += time_spent
+    avg_time = total_time_binary_search_recursive500 / 100
+    print(f"Binary Search Recursive with 500 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_binary_search_recursive1000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(1000)
+        start = time.time()
+        sorted_list = binary_search_recursive(mylist, -1)
+        time_spent = time.time() - start
+        total_time_binary_search_recursive1000 += time_spent
+    avg_time = total_time_binary_search_recursive1000 / 100
+    print(f"Binary Search Recursive with 1000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_binary_search_recursive5000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(5000)
+        start = time.time()
+        sorted_list = binary_search_recursive(mylist, -1)
+        time_spent = time.time() - start
+        total_time_binary_search_recursive5000 += time_spent
+    avg_time = total_time_binary_search_recursive5000 / 100
+    print(f"Binary Search Recursive with 5000 elements took {avg_time:10.7f} seconds to run, on average")
--- sort_compare.py
+++ sort_compare.py
@@ -1,30 +1,154 @@
-import argparse
-# other imports go here
-
+import time
 import random
 
 def get_me_random_list(n):
-    """Generate list of n elements in random order
-    
-    :params: n: Number of elements in the list
-    :returns: A list with n elements in random order
-    """
     a_list = list(range(n))
     random.shuffle(a_list)
     return a_list
-    
 
 def insertion_sort(a_list):
-    pass
+    begin = time.time()
+    for index in range(1, len(a_list)):
 
+        current_value = a_list[index]
+        position = index
+
+        while position > 0 and a_list[position - 1] > current_value:
+            a_list[position] = a_list[position - 1]
+            position = position - 1
+    end = time.time()
 
 def shell_sort(a_list):
-    pass
+    begin = time.time()
+    sublist_count = len(a_list) // 2
+    while sublist_count > 0:
+        for start_position in range(sublist_count):
+            gap_insertion_sort(a_list, start_position, sublist_count)
+
+        print("After increments of size", sublist_count, "The list is", a_list)
+
+        sublist_count = sublist_count // 2
+
+def gap_insertion_sort (a_list, start, gap):
+    for i in range(start + gap, len(a_list), gap):
+        current_value = a_list[i]
+        position = i
+        while position >= gap and a_list[position - gap] > current_value:
+            a_list[position] = a_list[position - gap]
+            position = position - gap
+
+        a_list[position] = current_value
 
 
 def python_sort(a_list):
-    pass
+    return sorted(a_list)
 
 if __name__ == "__main__":
-    """Main entry point"""
-    pass
+# Insertion Sort Execution Time Averages
+    total_time_insertion_sort500 = 0
+    for i in range(100):
+        mylist = get_me_random_list(500)
+        start = time.time()
+        sorted_list = insertion_sort(mylist)
+        time_spent = time.time() - start
+        total_time_insertion_sort500 += time_spent
+    avg_time = total_time_insertion_sort500 / 100
+    print(f"Insertion Sort with 500 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_insertion_sort1000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(1000)
+        start = time.time()
+        sorted_list = insertion_sort(mylist)
+        time_spent = time.time() - start
+        total_time_insertion_sort1000 += time_spent
+    avg_time = total_time_insertion_sort1000 / 100
+    print(f"Insertion Sort with 1000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_insertion_sort5000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(5000)
+        start = time.time()
+        sorted_list = insertion_sort(mylist)
+        time_spent = time.time() - start
+        total_time_insertion_sort5000 += time_spent
+    avg_time = total_time_insertion_sort1000 / 100
+    print(f"Insertion Sort with 5000 elements took {avg_time:10.7f} seconds to run, on average")
+
+
+# Shell Sort Execution Time Averages
+    total_time_shell_sort500 = 0
+    for i in range(100):
+        mylist = get_me_random_list(500)
+        start = time.time()
+        sorted_list = shell_sort(mylist)
+        time_spent = time.time() - start
+        total_time_shell_sort500 += time_spent
+    avg_time = total_time_shell_sort500 / 100
+    print(f"Shell Sort with 500 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_shell_sort1000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(1000)
+        start = time.time()
+        sorted_list = shell_sort(mylist)
+        time_spent = time.time() - start
+        total_time_shell_sort1000 += time_spent
+    avg_time = total_time_shell_sort1000 / 100
+    print(f"Shell Sort with 1000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_shell_sort5000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(5000)
+        start = time.time()
+        sorted_list = shell_sort(mylist)
+        time_spent = time.time() - start
+        total_time_shell_sort5000 += time_spent
+    avg_time = total_time_shell_sort1000 / 100
+    print(f"Shell Sort with 5000 elements took {avg_time:10.7f} seconds to run, on average")
+
+
+# Python Sort Execution Time Averages
+    total_time_python_sort500 = 0
+    for i in range(100):
+        mylist = get_me_random_list(500)
+        start = time.time()
+        sorted_list = python_sort(mylist)
+        time_spent = time.time() - start
+        total_time_python_sort500 += time_spent
+    avg_time = total_time_python_sort500 / 100
+    print(f"Python Sort with 500 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_python_sort1000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(1000)
+        start = time.time()
+        sorted_list = python_sort(mylist)
+        time_spent = time.time() - start
+        total_time_python_sort1000 += time_spent
+    avg_time = total_time_python_sort1000 / 100
+    print(f"Python Sort with 1000 elements took {avg_time:10.7f} seconds to run, on average")
+
+    total_time_python_sort5000 = 0
+    for i in range(100):
+        mylist = get_me_random_list(5000)
+        start = time.time()
+        sorted_list = python_sort(mylist)
+        time_spent = time.time() - start
+        total_time_python_sort5000 += time_spent
+    avg_time = total_time_python_sort5000 / 100
+    print(f"Python Sort with 5000 elements took {avg_time:10.7f} seconds to run, on average")
+
+
+
+# list_sizes = [500, 1000, 5000]
+#    for the_size in list_sizes:
+#        for i in range(100):
+#            count = 0
+#            mylist = get_me_random_list(the_size)
+#            start = time.time()
+#            sorted_list = python_sort(mylist)
+#            time_spent = time.time() - start
+#            total_time += time_spent
+#        avg_time = total_time / the_size
+#        print(f"Python Sort took {avg_time:10.7f} seconds to run, on average for {list_sizes}")
